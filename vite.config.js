60572fc8db06159f755b8b758f953ae2e0ba3fd5
KACPER KASZOWSKI <kacpkasz@gmail.com>
2023-11-12 16:32:28 +0100

    trying hard

--- vite.config.js
+++ vite.config.js
@@ -5,8 +5,8 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   server: {
     proxy: {
-     // '/v2/translate': 'https://api-free.deepl.com'
-      '/api': 'http://localhost:8080'
+     '/v2/translate': 'https://api-free.deepl.com'
+      // '/api': 'http://localhost:8080'
     }
   },
   plugins: [react()],
